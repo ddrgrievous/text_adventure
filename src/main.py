a1db47d43e234daec24b311a248e43cb566052fb
Jared <jared.wrathall@gmail.com>
2015-10-01 09:54:06 -0600

    push 2

---
+++ src/main.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct 1, 2015
+
+@author: Jared, Ethan
+'''
+
+if __name__ == '__main__':
+    game_in_play = True
+    while game_in_play == True:
+        input1 = raw_input("Make a choice: ")
+        if input1 == "quit":
+            game_in_play = False
+        
+        
