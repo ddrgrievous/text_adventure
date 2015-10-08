4a9e3079717bf0a4aed0c7cf1d05a3f98d1efbe8
Jared <jared.wrathall@gmail.com>
2015-10-08 09:05:04 -0600

    Input handle for city handler

--- src/CityHandler.py
+++ src/CityHandler.py
@@ -16,17 +16,21 @@ class CityHandler(object):
     def read_input(self, my_game, player_input):
          # show available commands
         if player_input.lower() == "help":
-            self.display_help()           
+            self.display_help() 
+            return True          
         # quit the game    
         elif player_input.lower() == "quit":
-            my_game.quit()   
+            my_game.quit()
+            return True   
         # display the map    
         elif player_input.lower() == "map":
             my_game.map.display(my_game.my_adventurer.location)
+            return True
         # leave the city    
-        if player_input.lower() == "leave":
+        elif player_input.lower() == "leave":
             return False
         else:
+            print ('Invalid Input')
             return True
             
         def display_help(self):
